21785742bda7f4f4c44e5de4143a3e91df2b1a46
otuormaa <oskari.kristian.tuormaa@cern.ch>
2020-09-24 13:59:23 +0200

    FailSim setup done

---
+++ failsim/checks.py
@@ -0,0 +1,139 @@
+"""
+File: checks.py
+Author: Oskari Tuormaa
+Email: oskari.kristian.tuormaa@cern.ch
+Github: https://github.com/Oskari-Tuormaa
+Description: TODO
+"""
+
+from typing import List
+import pymask
+import pandas
+
+
+class OpticsChecks:
+
+    """TODO: Docstring for OpticsChecks. """
+
+    def __init__(self, separation: bool = True,
+                 beta: bool = True,
+                 tol_sep: List = [1e-3, 10e-2, 1e-3, 1e-2],
+                 tol_beta: List = [1e-6, 1e-6, 1e-6, 1e-6],
+                 save_twiss_files: bool = True):
+        """TODO: Docstring for __init__.
+
+        Kwargs:
+            separation (TODO): TODO
+            beta (TODO): TODO
+            tol_sep (TODO): TODO
+            tol_beta (TODO): TODO
+            save_twiss_files (TODO): TODO
+
+        Returns: TODO
+
+        """
+        self.separation = separation
+        self.beta = beta
+        self.tol_sep = tol_sep
+        self.tol_beta = tol_beta
+        self.save_twiss_files = save_twiss_files
+
+    def check_separations(self, twiss_df_b1: pandas.DataFrame,
+                          twiss_df_b2: pandas.DataFrame,
+                          variables_dict: dict):
+        """TODO: Docstring for check_separations.
+
+        Args:
+            twiss_df_b1 (TODO): TODO
+            twiss_df_b2 (TODO): TODO
+            variables_dict (TODO): TODO
+
+        Returns: TODO
+
+        """
+        separations_to_check = []
+        for iip, ip in enumerate([1, 2, 5, 8]):
+            for plane in ['x', 'y']:
+                # (*) Adapet based on knob definitions
+                separations_to_check.append({
+                    'element_name': f'ip{ip}:1',
+                    'scale_factor': -2*1e-3,
+                    'plane': plane,
+                    # knobs like on_sep1h, onsep8v etc
+                    'varname': f'on_sep{ip}'+{'x': 'h', 'y': 'v'}[plane],
+                    'tol': self.tol_sep[iip]})
+        pymask.check_separations_against_madvars(separations_to_check,
+                                                 twiss_df_b1, twiss_df_b2,
+                                                 variables_dict)
+
+    def check_betas(self, beam: str, twiss_df: pandas.DataFrame,
+                    variables_dict: dict):
+        """TODO: Docstring for check_betas.
+
+        Args:
+            beam (TODO): TODO
+            twiss_df (TODO): TODO
+            variables_dict (TODO): TODO
+
+        Returns: TODO
+
+        """
+        twiss_value_checks = []
+        for iip, ip in enumerate([1, 2, 5, 8]):
+            for plane in ['x', 'y']:
+                # (*) Adapt based on knob definitions
+                twiss_value_checks.append({
+                    'element_name': f'ip{ip}:1',
+                    'keyword': f'bet{plane}',
+                    'varname': f'bet{plane}ip{ip}b{beam}',
+                    'tol': self.tol_beta[iip]})
+        pymask.check_twiss_against_madvars(twiss_value_checks, twiss_df,
+                                           variables_dict)
+
+    def __call__(self, mad: pymask.Madxp,
+                 sequences: List[str],
+                 twiss_name: str = 'twiss'):
+        """TODO: Docstring for __call__.
+
+        Args:
+            mad (TODO): TODO
+            sequences (TODO): TODO
+
+        Returns: TODO
+
+        """
+        var_dict = mad.get_variables_dicts()
+        twiss_dfs = {}
+        summ_dfs = {}
+        for ss in sequences:
+            mad.use(ss)
+            mad.twiss()
+            tdf = mad.get_twiss_df('twiss')
+            twiss_dfs[ss] = tdf
+            sdf = mad.get_summ_df('summ')
+            summ_dfs[ss] = sdf
+
+        if self.save_twiss_files:
+            for ss in sequences:
+                tt = twiss_dfs[ss]
+                tt.to_parquet(twiss_name + f'_seq_{ss}.parquet')
+
+        if self.beta:
+            for ss in sequences:
+                tt = twiss_dfs[ss]
+                self.check_betas(beam=ss[-1],
+                                 twiss_df=tt,
+                                 variables_dict=var_dict)
+            print('###### RESULT: IP beta test against knobs passed!')
+
+        if self.separation:
+            twiss_df_b1 = twiss_dfs['lhcb1']
+            twiss_df_b2 = twiss_dfs['lhcb2']
+            self.check_separations(twiss_df_b1, twiss_df_b2, var_dict)
+            print('###### RESULT: IP separation test against knobs passed!')
+
+        other_data = {}
+        other_data.update(var_dict)
+        other_data['summ_dfs'] = summ_dfs
+
+        return twiss_dfs, other_data
--- failsim/failsim.py
+++ failsim/failsim.py
@@ -8,6 +8,7 @@ Description: TODO
 
 
 from typing import Optional, List, Union
+from .checks import OpticsChecks
 import yaml
 import imp
 import os
@@ -19,7 +20,8 @@ class FailSim:
 
     """TODO: Docstring for FailSim. """
 
-    verbosity: str = 'echo, warn, info'
+    mad_verbosity: str = 'echo, warn, info'
+    failsim_verbosity: bool = True
 
     def __init__(self, mode: str,
                  save_intermediate_twiss: bool = True,
@@ -48,11 +50,30 @@ class FailSim:
         self._check_betas_at_ips = check_betas_at_ips
         self._check_separations_at_ips = check_separations_at_ips
 
+    def init_check(self):
+        """TODO: Docstring for init_check.
+        Returns: TODO
+
+        """
+        if self.failsim_verbosity:
+            print('FailSim -> Initializing check class')
+
+        self._check = OpticsChecks(
+            separation=self._check_separations_at_ips,
+            beta=self._check_betas_at_ips,
+            tol_sep=self._tol_sep,
+            tol_beta=self._tol_beta,
+            save_twiss_files=self._save_intermediate_twiss
+        )
+
     def load_metadata(self):
         """TODO: Docstring for load_metadata.
         Returns: TODO
 
         """
+        if self.failsim_verbosity:
+            print('FailSim -> Loading metadata')
+
         metadata_stream = pkg_resources.resource_stream(__name__,
                                                         'data/metadata.yaml')
         self._metadata = yaml.safe_load(metadata_stream)
@@ -67,7 +88,40 @@ class FailSim:
         Returns: TODO
 
         """
+        if self.failsim_verbosity:
+            print('FailSim -> Setting sequence: %s' % sequence_key)
+
         self._seq_key = sequence_key
+        self._custom_sequence = False
+
+    def select_custom_sequence(self, sequence_paths: List[str],
+                               run_version: int = 0,
+                               hllhc_version: float = 0.0):
+        """TODO: Docstring for select_custom_sequence.
+
+        Args:
+            sequence_path (TODO): TODO
+            sequence_path (TODO): TODO
+            sequence_path (TODO): TODO
+
+        Kwargs:
+            run_version (TODO): TODO
+            hllhc_version (TODO): TODO
+
+        Returns: TODO
+
+        """
+        if self.failsim_verbosity:
+            print("""FailSim -> Setting custom sequence:
+                  \tPath: %s
+                  \trun_version: %d
+                  \thllhc_version: %.1f""" % (sequence_paths, run_version,
+                                              hllhc_version))
+
+        self._sequence_paths = sequence_paths
+        self._run_version = run_version
+        self._hllhc_version = hllhc_version
+        self._custom_sequence = True
 
     def select_optics(self, optics_key: str):
         """TODO: Docstring for select_optics.
@@ -78,7 +132,30 @@ class FailSim:
         Returns: TODO
 
         """
+        if self.failsim_verbosity:
+            print('FailSim -> Setting optics: %s' % optics_key)
+
         self._opt_key = optics_key
+        self._custom_optics = False
+
+    def select_custom_optics(self, optics_path: str,
+                             optics_type: str,
+                             beta_ip5: float):
+        """TODO: Docstring for select_custom_optics.
+
+        Args:
+            optics_path (TODO): TODO
+
+        Returns: TODO
+
+        """
+        if self.failsim_verbosity:
+            print("FailSim -> Setting custom optics: %s" % optics_path)
+
+        self._opt_path = optics_path
+        self._opt_type = optics_type
+        self._beta_ip5 = beta_ip5
+        self._custom_optics = True
 
     def init_mad_instance(self):
         """TODO: Docstring for init_mad_instance.
@@ -87,6 +164,9 @@ class FailSim:
             None
 
         """
+        if self.failsim_verbosity:
+            print('FailSim -> Initializing MAD instance')
+
         self._mad = pm.Madxp()
 
     def mad_call_file(self, file_path: str):
@@ -98,7 +178,10 @@ class FailSim:
         Returns: TODO
 
         """
-        self._mad.input('option, %s' % self.verbosity)
+        if self.failsim_verbosity:
+            print('FailSim -> Calling MAD with file: %s' % file_path)
+
+        self._mad.input('option, %s' % self.mad_verbosity)
         self._mad.call(file_path)
 
     def fetch_mode_configuration(self):
@@ -107,6 +190,9 @@ class FailSim:
         Returns: TODO
 
         """
+        if self.failsim_verbosity:
+            print('FailSim -> Fetching mode configuration')
+
         (self._beam_to_configure,
          self._sequences_to_check,
          self._sequence_to_track,
@@ -117,23 +203,39 @@ class FailSim:
          self._force_disable_check_separations_at_ips,
          ) = pm.get_pymask_configuration(self._mode)
 
+        if self._force_disable_check_separations_at_ips:
+            self._check_separations_at_ips = False
+            self._check.check_separations = False
+
     def load_sequences_and_optics(self,
                                   pre_scripts: Optional[List[str]] = None,
                                   post_scripts: Optional[List[str]] = None):
         """TODO: Docstring for load_sequences_and_optics.
 
         Kwargs:
-            lhc_version (TODO): TODO
-            hllhc_version (TODO): TODO
             pre_scripts (TODO): TODO
             post_scripts (TODO): TODO
 
         Returns: TODO
 
         """
+        if self.failsim_verbosity:
+            print('FailSim -> Loading sequence and optics')
+
         # Load sequence and optics data
-        seq_data = self._metadata['SEQUENCES'][self._seq_key]
-        opt_data = self._metadata['OPTICS'][self._opt_key]
+        if self._custom_sequence:
+            seq_data = dict(sequence_filenames=self._sequence_paths,
+                            run=self._run_version,
+                            version=self._hllhc_version)
+        else:
+            seq_data = self._metadata['SEQUENCES'][self._seq_key]
+
+        if self._custom_optics:
+            opt_data = dict(type=self._opt_type,
+                            beta_ip5=self._beta_ip5,
+                            strength_file=self._opt_path)
+        else:
+            opt_data = self._metadata['OPTICS'][self._opt_key]
 
         # Set machine versions
         self._mad.input('ver_lhc_run = %s' % seq_data['run'])
@@ -146,7 +248,10 @@ class FailSim:
 
         # Run all sequences
         for sequence in seq_data['sequence_filenames']:
-            seq_path = pkg_resources.resource_filename(__name__, sequence)
+            if self._custom_sequence:
+                seq_path = sequence
+            else:
+                seq_path = pkg_resources.resource_filename(__name__, sequence)
             self.mad_call_file(seq_path)
 
         # Load optics
@@ -171,6 +276,9 @@ class FailSim:
         Returns: TODO
 
         """
+        if self.failsim_verbosity:
+            print('FailSim -> Making sequence thin')
+
         # If no script_path given, do nominal slice
         if script_path is None:
             macro_path = pkg_resources.resource_filename(__name__,
@@ -191,6 +299,9 @@ class FailSim:
         Returns: TODO
 
         """
+        if self.failsim_verbosity:
+            print('FailSim -> Cycling sequence')
+
         # Check whether multiple or single sequences should be cycled
         if isinstance(sequence, str):
             for seq in sequence:
@@ -209,6 +320,9 @@ class FailSim:
         Returns: TODO
 
         """
+        if self.failsim_verbosity:
+            print('FailSim -> Loading mask parameters')
+
         # If no specific path is given to mask_parameters,
         # assume that mask_parameters.py is in same directory
         if mask_path is None:
@@ -218,9 +332,6 @@ class FailSim:
             mask_params_source = imp.load_source(os.path.basename(mask_path),
                                                  os.path.abspath(mask_path))
             mask_parameters = mask_params_source.mask_parameters
-
-        if self._force_disable_check_separations_at_ips:
-            self._check_seperations_at_ips = False
 
         if not self._enable_bb_legacy and not self._enable_bb_python:
             mask_parameters['par_on_bb_switch'] = 0.
@@ -234,6 +345,9 @@ class FailSim:
         Returns: TODO
 
         """
+        if self.failsim_verbosity:
+            print('FailSim -> Calling preparation and beam modules')
+
         submod_prep = pkg_resources.resource_filename('pymask', '../submodule'
                                                       '_01a_preparation.madx')
         submod_beam = pkg_resources.resource_filename('pymask', '../submodule'
@@ -241,6 +355,9 @@ class FailSim:
 
         self.mad_call_file(submod_prep)
         self.mad_call_file(submod_beam)
+
+        self._check(self._mad, self._sequences_to_check,
+                    twiss_name='twiss_from_optics')
 
     def twiss(self, sequence: Union[List, str]):
         """TODO: Docstring for twiss.
@@ -251,6 +368,9 @@ class FailSim:
         Returns: TODO
 
         """
+        if self.failsim_verbosity:
+            print('FailSim -> Doing twiss')
+
         if isinstance(sequence, list):
             res = dict()
             for seq in sequence:
@@ -266,3 +386,83 @@ class FailSim:
             twiss_df = self._mad.get_twiss_df('twiss')
             summ_df = self._mad.get_summ_df('summ')
             return (twiss_df, summ_df)
+
+    def load_knob_parameters(self, knob_path: Optional[str] = None):
+        """TODO: Docstring for load_knob_parameters.
+
+        Kwargs:
+            knob_path (TODO): TODO
+
+        Returns: TODO
+
+        """
+        if self.failsim_verbosity:
+            print('FailSim -> Loading knob parameters')
+
+        # If no specific path is given to knob_parameters,
+        # assume that knob_parameters.py is in same directory
+        if knob_path is None:
+            from knob_parameters import knob_parameters
+        else:
+            # Load mask_parameters from path
+            knob_params_source = imp.load_source(os.path.basename(knob_path),
+                                                 os.path.abspath(knob_path))
+            knob_parameters = knob_params_source.knob_parameters
+
+        # PATCH!!!!!!! for leveling not working for b4
+        if self._mode == 'b4_without_bb':
+            knob_parameters['par_sep8'] = -0.03425547139366354
+            knob_parameters['par_sep2'] = 0.14471680504084292
+
+        self._mad.set_variables_from_dict(params=knob_parameters)
+
+        # Set IP knobs
+        self._mad.globals['on_x1'] = knob_parameters['par_x1']
+        self._mad.globals['on_sep1'] = knob_parameters['par_sep1']
+        self._mad.globals['on_x2'] = knob_parameters['par_x2']
+        self._mad.globals['on_sep2'] = knob_parameters['par_sep2']
+        self._mad.globals['on_x5'] = knob_parameters['par_x5']
+        self._mad.globals['on_sep5'] = knob_parameters['par_sep5']
+        self._mad.globals['on_x8'] = knob_parameters['par_x8']
+        self._mad.globals['on_sep8'] = knob_parameters['par_sep8']
+        self._mad.globals['on_a1'] = knob_parameters['par_a1']
+        self._mad.globals['on_o1'] = knob_parameters['par_o1']
+        self._mad.globals['on_a2'] = knob_parameters['par_a2']
+        self._mad.globals['on_o2'] = knob_parameters['par_o2']
+        self._mad.globals['on_a5'] = knob_parameters['par_a5']
+        self._mad.globals['on_o5'] = knob_parameters['par_o5']
+        self._mad.globals['on_a8'] = knob_parameters['par_a8']
+        self._mad.globals['on_o8'] = knob_parameters['par_o8']
+        self._mad.globals['on_crab1'] = knob_parameters['par_crab1']
+        self._mad.globals['on_crab5'] = knob_parameters['par_crab5']
+        self._mad.globals['on_disp'] = knob_parameters['par_on_disp']
+
+        # A check
+        if self._mad.globals.nrj < 500:
+            assert knob_parameters['par_on_disp'] == 0
+
+        # Spectrometers at experiments
+        if knob_parameters['par_on_alice'] == 1:
+            self._mad.globals.on_alice = 7000./self._mad.globals.nrj
+        if knob_parameters['par_on_lhcb'] == 1:
+            self._mad.globals.on_lhcb = 7000./self._mad.globals.nrj
+
+        # Solenoids at experiments
+        self._mad.globals.on_sol_atlas = knob_parameters['par_on_sol_atlas']
+        self._mad.globals.on_sol_cms = knob_parameters['par_on_sol_cms']
+        self._mad.globals.on_sol_alice = knob_parameters['par_on_sol_alice']
+
+    def crossing_save(self, store_optics: bool = True):
+        """TODO: Docstring for crossing_save.
+
+        Kwargs:
+            store_optics (TODO): TODO
+
+        Returns: TODO
+
+        """
+        self._mad.input('exec, crossing_save')
+        if store_optics:
+            mod_path = pkg_resources.resource_filename('pymask', '../submodule'
+                                                       '_01e_final.madx')
+            self._mad.call(mod_path)
