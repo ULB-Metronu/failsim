efc556109faa459905a54378d6f1aac6842e534c
otuormaa <oskari.kristian.tuormaa@cern.ch>
2020-10-15 15:56:57 +0200

    Added plotly as requirement

--- setup.py
+++ setup.py
@@ -6,37 +6,36 @@ import ast
 
 def get_version_from_init():
     """Obtain library version from main init."""
-    init_file = os.path.join(
-        os.path.dirname(__file__), 'failsim', '__init__.py'
-    )
+    init_file = os.path.join(os.path.dirname(__file__), "failsim", "__init__.py")
     with open(init_file) as fd:
         for line in fd:
-            if line.startswith('__version__'):
-                return ast.literal_eval(line.split('=', 1)[1].strip())
+            if line.startswith("__version__"):
+                return ast.literal_eval(line.split("=", 1)[1].strip())
 
 
-with open('README.md') as f:
+with open("README.md") as f:
     readme = f.read()
 
-with open('LICENSE') as f:
+with open("LICENSE") as f:
     lic = f.read()
 
 
 setup(
-    name='failsim',
+    name="failsim",
     version=get_version_from_init(),
-    description='LHC Fast failure simulation toolkit',
+    description="LHC Fast failure simulation toolkit",
     long_description=readme,
-    author='Cédric Hernaslteens, Oskari Tuormaa',
-    author_email='cedric.hernalsteens@cern.ch, oskari.kristian.tuormaa@cern.ch',
-    url='https://gitlab.cern.ch/machine-protection/fast-beam-failures',
+    author="Cédric Hernaslteens, Oskari Tuormaa",
+    author_email="cedric.hernalsteens@cern.ch, oskari.kristian.tuormaa@cern.ch",
+    url="https://gitlab.cern.ch/machine-protection/fast-beam-failures",
     license=lic,
-    packages=find_packages(exclude=('tests', 'docs', 'examples')),
+    packages=find_packages(exclude=("tests", "docs", "examples")),
     install_requires=[
-        'numpy',
-        'pandas',
-        'pymask',
-        'pyyaml',
+        "numpy",
+        "pandas",
+        "pymask",
+        "pyyaml",
+        "plotly",
     ],
-    package_data={'failsim': ['data']},
+    package_data={"failsim": ["data"]},
 )
